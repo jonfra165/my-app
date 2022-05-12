070ad9c47b3466ff355bb03101e26bdfbe19ed91
jonfra165 <jonna.franzen@hotmail.com>
2022-05-12 16:59:17 +0200

    Lagt till resterande funktioner

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function Movie(props) {
+    function getStars(number){
+        let stars = [];
+        for(var i = 0; i < number; i++) {
+            stars.push(<img src="/star.png" alt="Star" />);
+        }
+        return stars;
+        
+    };
     return (
         <li className="list-group-item">
             { props.item.title }
+            {getStars(props.item.rating)}
             <button className="btn btn-sm btn-danger float-end" onClick={() => {props.deleteItem(props.item.id)}}>X</button>
         </li>
     )
--- src/MovieList.js
+++ src/MovieList.js
@@ -2,29 +2,66 @@ import React, { useState, useRef } from 'react';
 import Movie from './Movie';
 
 export default function MovieList() {
-    const [movies, setMovie] = useState([]);
-    const inputRef = useRef();
+  const [movies, setMovie] = useState([]);
+  const inputRef = useRef();
+  const ratingRef = useRef();
+  const [validTitle, setValidTitle] = useState(false);
+  const [validRating, setValidRating] = useState(false);
 
-    function addMovie(event) {        
-          const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
-          setMovie([...movies, {
-              id: newId,
-              title: inputRef.current.value,
-          }])
 
-          inputRef.current.value = "";
-        
-    }
-    function deleteItem(id) {
-      setMovie(movies.filter((item) => item.id !== id));
+  function addMovie(event) {   
+    if (validTitle && validRating) {
+      const newId = movies.length > 0 ? movies[movies.length - 1].id + 1 : 1;
+      setMovie([...movies, {
+        id: newId,
+        title: inputRef.current.value,
+        rating: ratingRef.current.value
+      }])
+
+      inputRef.current.value = "";
+    } else {
+      alert("Fyll i för fan!")
+    }     
   }
+    
+  function deleteItem(id) {
+    setMovie(movies.filter((item) => item.id !== id));
+  }
+   
+  function isValid(valid) {
+    return 'form-control '+(valid ?'':'is-invalid');
+  }
+
+  const updateValidationTitle = () => {
+    setValidTitle(inputRef.current.value != "");
+  }
+  const updateValidationRate = () => {
+    setValidRating(ratingRef.current.value != "0");
+  }
+
+  function sortByTitle() {
+    const sortedMovies = [...movies].sort((a, b) => {
+        return a.title.toLowerCase() > b.title.toLowerCase() ? 1 : -1;
+    });
+
+    setMovie(sortedMovies);
+}
+function sortByRating() {
+  const sortedMovies = [...movies].sort((a, b) => {
+      return a.rating.toLowerCase() > b.rating.toLowerCase() ? 1 : -1;
+  });
+
+  setMovie(sortedMovies);
+}
 
   return (
       <div>
-          <input className="form-control" ref={inputRef} placeholder="Add a new movie here..."  />
+
+          <label for="rating-field">Titel:</label>
+          <input className={isValid(validTitle)} ref={inputRef} placeholder="Add a new movie here..." onKeyUp={updateValidationTitle}  />
           <label for="rating-field">Betyg:</label>
 
-                <select type="text" id="rating-field" className="form-control">
+                <select type="text" id="rating-field" className={isValid(validRating)} ref={ratingRef} onChange={updateValidationRate}>
                     <option value="0">Välj betyg här...</option>
                     <option value="1">1</option>
                     <option value="2">2</option>
@@ -33,7 +70,9 @@ export default function MovieList() {
                     <option value="5">5</option>
                 </select>
 
-                <input type="submit" class="btn btn-success mt-3" value="Spara film" onClick={addMovie}/>
+                <input type="submit" className="btn btn-success mt-3" value="Spara film" onClick={addMovie}/>
+                <input className="btn btn-primary mt-3" value="Sortera på titel" onClick={sortByTitle}/>
+                <input className="btn btn-primary mt-3" value="Sortera på betyg" onClick={sortByRating}/>
           <ul className="list-group">
               {movies.map(movie => <Movie key={movie.id} item={movie} deleteItem={deleteItem} />)}
           </ul>
